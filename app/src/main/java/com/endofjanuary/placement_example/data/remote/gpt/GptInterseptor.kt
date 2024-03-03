6f791df672a80534f03b43d91a3e920aac0e6813
ScheznikL <luybabuharova@gmail.com>
2024-03-03 10:31:18 +0200

    commit

--- app/src/main/java/com/endofjanuary/placement_example/data/remote/gpt/GptInterseptor.kt
+++ app/src/main/java/com/endofjanuary/placement_example/data/remote/gpt/GptInterseptor.kt
@@ -1,11 +1,12 @@
 package com.endofjanuary.placement_example.data.remote.gpt
 
+import OPENAI_API_KEY
 import okhttp3.Interceptor
 import okhttp3.Response
 class AuthTokenGptInterseptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        val token = "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"
+        val token = OPENAI_API_KEY
         if (!token.isNullOrBlank()) {
             request = request.newBuilder()
                 .addHeader("Authorization", "Bearer $token")
--- app/src/main/java/com/endofjanuary/placement_example/data/remote/meshy/Interseptor.kt
+++ app/src/main/java/com/endofjanuary/placement_example/data/remote/meshy/Interseptor.kt
@@ -1,12 +1,13 @@
 package com.endofjanuary.placement_example.data.remote.meshy
 
+import MESHY_API_KEY
 import okhttp3.Interceptor
 import okhttp3.Response
 
 class AuthTokenInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var request = chain.request()
-        val token = "msy_mTSwQvvjyVVw1LnJbH3vZLrPkZahSxajpC34"
+        val token = MESHY_API_KEY
         if (!token.isNullOrBlank()) {
             request = request.newBuilder()
                 .addHeader("Authorization", "Bearer $token")
